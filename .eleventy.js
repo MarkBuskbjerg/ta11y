32a2434eef8c3479f3ad72e09f8fe1f85cf99433
Mark Buskbjerg <mark.buskbjerg@gmail.com>
2020-10-20 21:43:19 +0200

    is stuff really published later now?

--- .eleventy.js
+++ .eleventy.js
@@ -6,13 +6,22 @@ module.exports = function (eleventyConfig) {
 
 	eleventyConfig.addCollection('posts', collection => {
 		// Check if draft is true in frontmatter - used to exclude from the collection
-		const isLive = post => !post.data.draft;
-		// TODO: Check if this is on production or development environment
-		// TODO: Figure out a way to get only from posts folder
-		return collection.getFilteredByGlob('**/*.njk')
-			.filter(isLive)
-			.filter(item => !item.inputPath.includes('index.njk'))
-			.reverse();
+		const isLive = post => !post.data.draft && post.date <= new Date();
+		const isIndex = post => !post.inputPath.includes('index.njk');
+		// Return all posts - including drafts - when in development environment
+		if (process.env.ELEVENTY_ENV !== 'production')
+			return collection
+				.getFilteredByGlob('**/*.njk')
+				.filter(isIndex)
+				.reverse();
+		else {
+			// TODO: Figure out a way to get only from posts folder
+			return collection
+				.getFilteredByGlob('**/*.njk')
+				.filter(isLive)
+				.filter(isIndex)
+				.reverse();
+		}
 	});
 
 
