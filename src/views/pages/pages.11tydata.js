25800bc9b23a2332e04921402f57db882e48f4b8
Mark Buskbjerg <mark.buskbjerg@gmail.com>
2023-04-25 15:31:55 +0200

    Update all permalinks without server-restart
    I had an issues in my pages.11tydata.js where the code to rewrite the path to the root only worked on the initial build.
    
    This is updated to use 'path' instead of 'filePathStem' to rewrite the permalink.
    
    At the same time I changed from .html to / to enforce trailing slash on all pages.

--- src/views/pages/pages.11tydata.js
+++ src/views/pages/pages.11tydata.js
@@ -1,11 +1,15 @@
+const path = require("path");
+
 module.exports = async () => {
-	return {
-		layout: 'page',
-		tags: 'page',
-		eleventyComputed: {
-			permalink: (data) => {
-				return data.page.filePathStem.replace('/pages/', '/') + '.html';
-			},
-		},
-	};
+  return {
+    layout: "page",
+    tags: "page",
+    eleventyComputed: {
+      permalink: (data) => {
+        const rootPath = path.join(__dirname, "../");
+        const pagePath = path.relative(rootPath, data.page.inputPath);
+        return "/" + path.parse(pagePath).name + "/";
+      },
+    },
+  };
 };
