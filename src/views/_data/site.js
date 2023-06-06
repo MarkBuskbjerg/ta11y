01b4ca7a5535449aece6464bcf9a6cc1b0292cd3
Mark Buskbjerg <mark.buskbjerg@gmail.com>
2023-06-06 06:54:18 +0200

    Update site.js
    Removed the end slash to fix the double-slash issues in my canonical tags.

--- src/views/_data/site.js
+++ src/views/_data/site.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: "Ta11y.app – 11ty starter project",
-  url: "https://ta11y.app/",
+  url: "https://ta11y.app",
   language: "en",
   description: "I am writing about my experiences as a naval navel-gazer.",
   author: {
