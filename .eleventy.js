fc6c70d93b7e0cd0fa3469644cef9d50df9049ec
Mark Buskbjerg <mark.buskbjerg@gmail.com>
2023-10-20 00:00:13 +0200

    let's test the auto-generated OG images

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,6 @@
 /* eslint-disable */
+const fs = require('fs');
+const Image = require('@11ty/eleventy-img');
 const { format } = require('date-fns');
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 const tailwind = require('tailwindcss');
@@ -77,10 +79,55 @@ module.exports = function (eleventyConfig) {
 		return (tags || []).filter((tag) => ['page', 'post'].indexOf(tag) === -1);
 	});
 
+	// A filter to split the lines in the Open Graph generated SVG preview image
+	eleventyConfig.addFilter('splitlines', function (input) {
+		const parts = input.split(' ');
+		const lines = parts.reduce(function (prev, current) {
+			if (!prev.length) {
+				return [current];
+			}
+
+			let lastOne = prev[prev.length - 1];
+
+			if (lastOne.length + current.length > 19) {
+				return [...prev, current];
+			}
+
+			prev[prev.length - 1] = lastOne + ' ' + current;
+
+			return prev;
+		}, []);
+
+		return lines;
+	});
+
 	// Date filters
 	// -----------------------------
 	eleventyConfig.addFilter('datefmt', (contentDate) => {
 		return format(contentDate, "LLL d'th' - yyyy");
+	});
+
+	// Convert SVG previews of posts to JPG
+	eleventyConfig.on('afterBuild', () => {
+		const socialPreviewImagesDir = '_site/img/social-preview-images/';
+		fs.readdir(socialPreviewImagesDir, function (err, files) {
+			if (files.length > 0) {
+				files.forEach(function (filename) {
+					if (filename.endsWith('.svg')) {
+						let imageUrl = socialPreviewImagesDir + filename;
+						Image(imageUrl, {
+							formats: ['jpeg'],
+							outputDir: './' + socialPreviewImagesDir,
+							filenameFormat: function (id, src, width, format, options) {
+								let outputFilename = filename.substring(0, filename.length - 4);
+
+								return `${outputFilename}.${format}`;
+							},
+						});
+					}
+				});
+			}
+		});
 	});
 
 	// BrowserSync configuration
